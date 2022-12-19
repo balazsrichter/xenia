b4c67e99b5e3febd4f06f4bd5a5f9b395cb141d8
Richter Balázs <115174953+balazsrichter@users.noreply.github.com>
2022-12-19 04:03:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -9,31 +9,31 @@ let sd=0
 let se=0
 
 if (a == 1) {document.getElementById("v1").innerHTML ="Jó válasz!", sa++, document.getElementById("v1").style.color="lime"}
-else {document.getElementById("v1").innerHTML = xenia.vezeteknev, document.getElementById("v1").style.color="red"}	
+else {document.getElementById("v1").innerHTML = xenia.vezeteknev, document.getElementById("v1").style.color="yellow"}	
 
 if (b == 1) {document.getElementById("v2").innerHTML ="Jó válasz!", sb++, document.getElementById("v2").style.color="lime"}
-else {document.getElementById("v2").innerHTML = xenia.szulev, document.getElementById("v2").style.color="red"}	
+else {document.getElementById("v2").innerHTML = xenia.szulev, document.getElementById("v2").style.color="yellow"}	
 
 if (c == 1) {document.getElementById("v3").innerHTML ="Jó válasz!", sc++, document.getElementById("v3").style.color="lime"}
-else {document.getElementById("v3").innerHTML = xenia.regio, document.getElementById("v3").style.color="red"}
+else {document.getElementById("v3").innerHTML = xenia.regio, document.getElementById("v3").style.color="yellow"}
 	
 if (d == 1) {document.getElementById("v4").innerHTML ="Jó válasz!", sd++, document.getElementById("v4").style.color="lime"}
-else {document.getElementById("v4").innerHTML = xenia.klub, document.getElementById("v4").style.color="red"}
+else {document.getElementById("v4").innerHTML = xenia.klub, document.getElementById("v4").style.color="yellow"}
 	
 if (e == 1) {document.getElementById("v5").innerHTML ="Jó válasz!", se++, document.getElementById("v5").style.color="lime"}
-else {document.getElementById("v5").innerHTML = xenia.kiado, document.getElementById("v5").style.color="red"}
+else {document.getElementById("v5").innerHTML = xenia.kiado, document.getElementById("v5").style.color="yellow"}
 
 let s=sa+sb+sc+sd+se
 
 document.getElementById("szamlalo").innerHTML = s +"/5"
 	
-if (s == 5) {document.getElementById("gratula").innerHTML ="Gratulálunk, igazi Xenia rajongó vagy!", document.getElementById("gratula").style.color="lime", document.getElementById("szamlalo").style.color="lime"}
+if (s == 5) {document.getElementById("gratula").innerHTML ="Igazi Xenia rajongó vagy!", document.getElementById("gratula").style.color="lime", document.getElementById("szamlalo").style.color="lime"}
 	
-else if (s == 4) {document.getElementById("gratula").innerHTML ="Majdnem hibátlan!", document.getElementById("gratula").style.color="yellow", document.getElementById("szamlalo").style.color="yellow"}
+else if (s == 4) {document.getElementById("gratula").innerHTML ="Majdnem hibátlan!", document.getElementById("gratula").style.color="turquoise", document.getElementById("szamlalo").style.color="turquoise"}
 	
-else if (s<4 && s>1) {document.getElementById("gratula").innerHTML ="Valamennyire ismered...", document.getElementById("gratula").style.color="orange", document.getElementById("szamlalo").style.color="orange"}
+else if (s<4 && s>1) {document.getElementById("gratula").innerHTML ="Valamennyire ismered...", document.getElementById("gratula").style.color="yellow", document.getElementById("szamlalo").style.color="yellow"}
 	
-else {document.getElementById("gratula").innerHTML ="Ismerd meg jobban!", document.getElementById("gratula").style.color="red", document.getElementById("szamlalo").style.color="red"}
+else {document.getElementById("gratula").innerHTML ="Ismerd meg jobban!", document.getElementById("gratula").style.color="orange", document.getElementById("szamlalo").style.color="orange"}
 }
 
 
@@ -47,3 +47,7 @@ let vkiado = document.getElementById("kiado").checked
 kviz(vnev, vszulev, vregio, vklub, vkiado)	
 } 
 
+function hiba() 
+{
+window.alert("Az űrlap nem működik, megkeresés a láblécben található hivatalos elérhetőségein lehetséges!")
+}
